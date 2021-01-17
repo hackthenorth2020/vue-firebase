93906a4913e194b2aa205de5be15eaab5010137c
Aman <aman.s.chhina@gmail.com>
2021-01-17 10:34:48 -0500

    Created pairing page.

--- pages/pairing.vue
+++ pages/pairing.vue
@@ -1,9 +1,33 @@
 <template>
-  <div><h1>Pairing Page</h1></div>
+  <div>
+    <v-card>
+      <v-card-title>Send a small introduction and availability!</v-card-title>
+      <v-text-field label="Introduction and availbility" outlined>
+      </v-text-field>
+      <v-btn
+        primary
+        large
+        block
+        class="px-4, blue--text"
+        outlined
+        @click="loginUser()"
+      >
+        Login
+      </v-btn>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    loginUser() {
+      this.$alert("Successfully Paired", "Success", "success").then(() => {
+        this.$router.push("/dashboard");
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
