8332a466d0f2237acdccff7e7e7533c1b5f69326
hjanday <jandaypreet@gmail.com>
2021-01-17 02:39:14 -0500

    Made signin and signout ui changes, working on homepage and the scheduling system

---
+++ pages/schedule.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container class="ma-6">
+    <v-card class="pa-6" outlined>
+      <v-card-title class="justify-center">
+        <h3>What Days Are You Available</h3>
+      </v-card-title>
+      <v-form>
+        <v-text-field
+          v-model="text"
+          label="Date"
+          placeholder="Monday, etc.."
+          prepend-inner-icon="mdi-calendar"
+          outlined
+        />
+      </v-form>
+  </v-card>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    data () {
+        
+      return {
+        picker: new Date().toISOString().substr(0, 10),
+        
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
