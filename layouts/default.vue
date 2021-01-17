bffba6c5434fea087ecdb5bdfa93e1b37fcdfebd
Aman <aman.s.chhina@gmail.com>
2021-01-16 21:47:54 -0500

    Removed useless buttons for minus and left/right expant.

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,18 +36,7 @@
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
-      </v-btn>
-      <!-- <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn> -->
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+
       <v-toolbar-title v-text="title" />
 
       <div class="flex-grow-1"></div>
